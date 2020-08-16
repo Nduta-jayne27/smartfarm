d433f92e95fad4fd76fb78dbf7f42e416dedaf22
Nduta-jayne27 <jaynemanyi@gmail.com>
2020-08-16 21:25:44 +0300

    change views and index

--- mkulimaBora/models.py
+++ mkulimaBora/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class farmdata(models.Model):
+	humidity = models.CharField(max_length=128)
+    temparature = models.CharField(max_length=128)
+	time = models.DateTimeField( auto_now=True)
