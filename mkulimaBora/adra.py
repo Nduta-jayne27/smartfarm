ac5a8377a27c5a5a5df8c253a1637db60ee54599
kamauchege159 <kamauchege159@gmail.com>
2020-08-16 20:32:53 +0300

    change of format

--- mkulimaBora/adra.py
+++ mkulimaBora/adra.py
@@ -5,7 +5,9 @@ pin=4
 while True:
 	humidity,temparature=Adafruit_DHT.read(sensor,pin)
 	if humidity is not None and temparature is not None:
-		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temparature, humidity))
+		hum= '{0:0.1f}%'.format(humidity)
+		temp= '{0:0.1f}*'.format(temparature)
+		print( hum + temp)
 	else:
 		print('Failed to get reading. Try again!')
 	time.sleep(3)
