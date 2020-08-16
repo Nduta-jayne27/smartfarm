fea38f32e593e70b1e3c307a28d24458bcc17657
Nduta-jayne27 <jaynemanyi@gmail.com>
2020-08-16 21:04:58 +0300

    change views and index

--- mkulimaBora/views.py
+++ mkulimaBora/views.py
@@ -4,10 +4,25 @@ import time
 channel = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channel, GPIO.IN)
+
+import Adafruit_DHT
+sensor=Adafruit_DHT.DHT11
+pin=4
+
+while True:
+	humidity,temparature=Adafruit_DHT.read(sensor,pin)
+	if humidity is not None and temparature is not None:
+		hum= '{0:0.1f}%'.format(humidity)
+		temp= '{0:0.1f}*'.format(temparature)
+		print( hum + temp)
+	else:
+		print('Failed to get reading. Try again!')
+	time.sleep(3)
 # Create your views here.
 def jc(request):
-	return render(request,'index.html',{})
+	return render(request,'index.html',{'hum':hum,'temp':temp})
 def kc(request):
+
 	message=""
 	if GPIO.input(channel):
 		message="Water The Farm"
