b6e0887c53009f81cfc61079777a1b19442ad49a
Nduta-jayne27 <jaynemanyi@gmail.com>
2020-08-17 15:15:54 +0300

    photos

--- mkulimaBora/models.py
+++ mkulimaBora/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class farmdata(models.Model):
-	humidity = models.CharField(max_length=128) 
-	temparature = models.CharField(max_length=128)
+	humidity = models.CharField(max_length=128)
+    temparature = models.CharField(max_length=128)
 	time = models.DateTimeField( auto_now=True)
-	def __str__(self):
-		return str(self.time.strftime("%d/%m/%Y - %H:%M:%S"))
+    def __str__(self):
+		return str(self.time.strftime("%d%m%Y - %H:%M:%S"))
